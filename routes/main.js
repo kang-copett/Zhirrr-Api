e703ae27a5b80dcc223c3fb18a62e6c55d3f5941
kang-copett <83132608+kang-copett@users.noreply.github.com>
2021-04-25 05:52:56 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -16,10 +16,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'RafizqiBot',
+            namaowner: 'Rafizqi',
+            instagram: 'Rafizqi',
+            youtube : 'Rafizqi'
         }
     }
     res.json(config)
